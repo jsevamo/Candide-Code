e9a76bcb7dd8e477a6f7e7d3c7e9030eae710635
Sebastian Vargas <j.sevamo@gmail.com>
2018-04-04 11:59:00 -0500

    Looks like we are able to read AUs succesfully from a single XML. Hooray!

--- Assets/Scripts/ParseXML.cs
+++ Assets/Scripts/ParseXML.cs
@@ -15,7 +15,9 @@ public class ParseXML : MonoBehaviour {
 
     //---------------------------------------------------------------------
 
-    List<Dictionary<string, string>> AusDic;
+
+    public List<Vector3> destinationList = new List<Vector3>();
+    public List<int> idList = new List<int>();
 
     // Use this for initialization
     void Start () {
@@ -25,14 +27,14 @@ public class ParseXML : MonoBehaviour {
         candideVertexDic = parseVertices();
         //dictionary = candideVertexDic[1];
 
-        AusDic = parseAUs();
-        dictionary = AusDic[1];
+        /*AusDic = parseAUs();
+        dictionary = AusDic[0];
 
 
         Debug.Log(dictionary["x"]);
         Debug.Log(dictionary["y"]);
         Debug.Log(dictionary["z"]);
-        Debug.Log(dictionary["ID"]);
+        Debug.Log(dictionary["ID"]);*/
 
         /*Vector3 meat = new Vector3(float.Parse(dic["riddle"]), float.Parse(dic["ans"]), float.Parse(dic["test"]));
         Debug.Log(meat.y);*/
@@ -49,6 +51,66 @@ public class ParseXML : MonoBehaviour {
         }
 
 
+
+
+        //--------------------------------------------------------------
+
+
+        TextAsset txtXmlAsset = Resources.Load<TextAsset>("listaVertices");
+        var doc = XDocument.Parse(txtXmlAsset.text);
+
+        var allDict = doc.Element("document").Elements("Au");
+        List<Dictionary<string, string>> allTextDic = new List<Dictionary<string, string>>();
+
+
+        foreach (var oneDict in allDict)
+        {
+            var twoStrings = oneDict.Elements("Vertex");
+            //Debug.Log(twoStrings.ElementAt(0));
+
+            var twoStringsVertex = twoStrings.Elements("v");
+            //Debug.Log(twoStringsVertex.ElementAt(0));
+
+            var twoStringsVertex2 = twoStrings.Elements("id");
+
+            XElement element1 = twoStringsVertex.ElementAt(0);
+            XElement element2 = twoStringsVertex.ElementAt(1);
+            XElement element3 = twoStringsVertex.ElementAt(2);
+            XElement element4 = twoStringsVertex2.ElementAt(0);
+
+            string first = element1.ToString().Replace("<v>", "").Replace("</v>", "");
+            string second = element2.ToString().Replace("<v>", "").Replace("</v>", "");
+            string third = element3.ToString().Replace("<v>", "").Replace("</v>", "");
+            string fourth = element4.ToString().Replace("<id>", "").Replace("</id>", "");
+
+            //Debug.Log(first);
+
+            //Dictionary<int, Vector3> AUS = new Dictionary<int, Vector3>();
+
+            Vector3 moveTo = new Vector3(float.Parse(first), float.Parse(second), float.Parse(third));
+
+            //AUS.Add(int.Parse(fourth), int.Parse(first));
+
+            destinationList.Add(moveTo);
+            idList.Add(int.Parse(fourth));
+
+            
+            
+
+            /*Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("x", first);
+            dic.Add("y", second);
+            dic.Add("z", third);
+            dic.Add("ID", fourth);
+
+            //allTextDic.Add(dic);
+            //amountOfVertex++;*/
+        }
+
+
+        Debug.Log(destinationList[0] + " " +  idList[0]);
+        Debug.Log(destinationList.Count);
+
     }
 
     public List<Dictionary<string, string>> parseVertices()
@@ -61,14 +123,17 @@ public class ParseXML : MonoBehaviour {
         foreach (var oneDict in allDict)
         {
             var twoStrings = oneDict.Elements("v");
+            var twoStrings2 = oneDict.Elements("id");
+
             XElement element1 = twoStrings.ElementAt(0);
             XElement element2 = twoStrings.ElementAt(1);
             XElement element3 = twoStrings.ElementAt(2);
-            XElement element4 = twoStrings.ElementAt(3);
+            XElement element4 = twoStrings2.ElementAt(0);
+
             string first = element1.ToString().Replace("<v>", "").Replace("</v>", "");
             string second = element2.ToString().Replace("<v>", "").Replace("</v>", "");
             string third = element3.ToString().Replace("<v>", "").Replace("</v>", "");
-            string fourth = element4.ToString().Replace("<v>", "").Replace("</v>", "");
+            string fourth = element4.ToString().Replace("<id>", "").Replace("</id>", "");
 
             Dictionary<string, string> dic = new Dictionary<string, string>();
             dic.Add("x", first);
@@ -89,19 +154,24 @@ public class ParseXML : MonoBehaviour {
         TextAsset txtXmlAsset = Resources.Load<TextAsset>("listaVertices");
         var doc = XDocument.Parse(txtXmlAsset.text);
 
-        var allDict = doc.Element("document").Elements("Au").Elements("Vertex");
+        var allDict = doc.Element("document").Elements("Au");
         List<Dictionary<string, string>> allTextDic = new List<Dictionary<string, string>>();
+
+
         foreach (var oneDict in allDict)
         {
-            var twoStrings = oneDict.Elements("v");
+            var twoStrings = oneDict.Elements("Vertex");
+            //var twoStrings2 = oneDict.Elements("id");
+
             XElement element1 = twoStrings.ElementAt(0);
             XElement element2 = twoStrings.ElementAt(1);
             XElement element3 = twoStrings.ElementAt(2);
             XElement element4 = twoStrings.ElementAt(3);
+
             string first = element1.ToString().Replace("<v>", "").Replace("</v>", "");
             string second = element2.ToString().Replace("<v>", "").Replace("</v>", "");
             string third = element3.ToString().Replace("<v>", "").Replace("</v>", "");
-            string fourth = element4.ToString().Replace("<v>", "").Replace("</v>", "");
+            string fourth = element4.ToString().Replace("<id>", "").Replace("</id>", "");
 
             Dictionary<string, string> dic = new Dictionary<string, string>();
             dic.Add("x", first);
