8672e693deb864500a33f473c47cabfe2488e114
Sebastian Vargas <j.sevamo@gmail.com>
2018-04-04 11:04:26 -0500

    Getting the hang of the XML. Almost there!

--- Assets/Scripts/ParseXML.cs
+++ Assets/Scripts/ParseXML.cs
@@ -13,17 +13,26 @@ public class ParseXML : MonoBehaviour {
     public List<Vector4> candideVertexList = new List<Vector4>();
     int amountOfVertex;
 
+    //---------------------------------------------------------------------
+
+    List<Dictionary<string, string>> AusDic;
+
     // Use this for initialization
     void Start () {
 
         amountOfVertex = 0;
 
         candideVertexDic = parseVertices();
-        dictionary = candideVertexDic[1];
-        /*Debug.Log(dictionary["x"]);
+        //dictionary = candideVertexDic[1];
+
+        AusDic = parseAUs();
+        dictionary = AusDic[1];
+
+
+        Debug.Log(dictionary["x"]);
         Debug.Log(dictionary["y"]);
-        Debug.Log(dictionary["z"]);*/
-        //Debug.Log(dictionary["ID"]);
+        Debug.Log(dictionary["z"]);
+        Debug.Log(dictionary["ID"]);
 
         /*Vector3 meat = new Vector3(float.Parse(dic["riddle"]), float.Parse(dic["ans"]), float.Parse(dic["test"]));
         Debug.Log(meat.y);*/
@@ -47,7 +56,7 @@ public class ParseXML : MonoBehaviour {
         TextAsset txtXmlAsset = Resources.Load<TextAsset>("listaVertices");
         var doc = XDocument.Parse(txtXmlAsset.text);
 
-        var allDict = doc.Element("document").Elements("Vertex");
+        var allDict = doc.Element("document").Elements("Mesh").Elements("Vertex");
         List<Dictionary<string, string>> allTextDic = new List<Dictionary<string, string>>();
         foreach (var oneDict in allDict)
         {
@@ -69,6 +78,39 @@ public class ParseXML : MonoBehaviour {
 
             allTextDic.Add(dic);
             amountOfVertex++;
+        }
+
+        return allTextDic;
+
+    }
+
+    public List<Dictionary<string, string>> parseAUs()
+    {
+        TextAsset txtXmlAsset = Resources.Load<TextAsset>("listaVertices");
+        var doc = XDocument.Parse(txtXmlAsset.text);
+
+        var allDict = doc.Element("document").Elements("Au").Elements("Vertex");
+        List<Dictionary<string, string>> allTextDic = new List<Dictionary<string, string>>();
+        foreach (var oneDict in allDict)
+        {
+            var twoStrings = oneDict.Elements("v");
+            XElement element1 = twoStrings.ElementAt(0);
+            XElement element2 = twoStrings.ElementAt(1);
+            XElement element3 = twoStrings.ElementAt(2);
+            XElement element4 = twoStrings.ElementAt(3);
+            string first = element1.ToString().Replace("<v>", "").Replace("</v>", "");
+            string second = element2.ToString().Replace("<v>", "").Replace("</v>", "");
+            string third = element3.ToString().Replace("<v>", "").Replace("</v>", "");
+            string fourth = element4.ToString().Replace("<v>", "").Replace("</v>", "");
+
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("x", first);
+            dic.Add("y", second);
+            dic.Add("z", third);
+            dic.Add("ID", fourth);
+
+            allTextDic.Add(dic);
+           //amountOfVertex++;
         }
 
         return allTextDic;
